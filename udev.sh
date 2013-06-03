5bb9ab44a13d14eeb4baa7b0d667696109805812
Roman Frołow <r.frolow@samsung.com>
2013-06-03 13:43:59 +0200

    added comments for udev.sh

--- udev.sh
+++ udev.sh
@@ -1,1 +1,5 @@
+#http://serverfault.com/questions/192546/how-to-change-device-permissions-via-udev-in-rhel5
+#http://serverfault.com/questions/194600/udev-how-can-i-extend-a-default-rule-to-modify-the-ownership-of-a-symlink
+#https://www.virtualbox.org/ticket/5249
+#mv 10-usb.rules /etc/udev/rules.d/
 udevadm control --reload && udevadm trigger --verbose && ls -l /dev/bus/usb/*/*
