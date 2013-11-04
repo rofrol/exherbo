1a997aca4668794436467fb18a35b9d8ef21a437
Roman Frołow <rofrol@gmail.com>
2013-11-04 12:59:06 +0000

    sun jdk

--- java.sh
+++ java.sh
@@ -9,3 +9,10 @@ cave resolve jdk -x
 #<SardemFF7> icedtea is the only maintained one (either upstream and in Exherbo)
 #<SardemFF7> you can revive the sun-jdk-bin package from graveyard, and maintain it
 #then echo "dev-lang/icedtea7" >> /etc/paludis/package_mask.conf
+
+#sun
+# http://askubuntu.com/questions/56104/how-can-i-install-sun-oracles-proprietary-java-6-7-jre-or-jdk
+#download jdk
+#tar xf jdk*.tar.gz -C /usr/lib64
+#cd /usr/lib64/jdk*/bin
+#for i in *; do ln -sf `pwd`/$i /usr/bin/; done
