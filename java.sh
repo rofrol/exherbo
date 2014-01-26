c300dfc9aa0524380c28c005e6c31885d61a92d2
Roman Frołow <rofrol@gmail.com>
2014-01-26 12:27:45 +0000

    installing sun jdk with bash script, env.adoc for env things

--- java.sh
+++ java.sh
@@ -1,7 +1,11 @@
-echo "dev-lang/icedtea7 bootstrap" >> /etc/paludis/options.conf
-cave resolve jdk -x
+#!/usr/bin/env bash
+function log {
+	[ "$LOG" == "DEBUG" ] && echo "$@"
+}
+#echo "dev-lang/icedtea7 bootstrap" >> /etc/paludis/options.conf
+#cave resolve jdk -x
 #remvoe bootstrap flag
-cave resolve jdk -x
+#cave resolve jdk -x
 
 #http://lists.exherbo.org/pipermail/exherbo-dev/2012-September/001142.html
 
@@ -12,7 +16,37 @@ cave resolve jdk -x
 
 #sun
 # http://askubuntu.com/questions/56104/how-can-i-install-sun-oracles-proprietary-java-6-7-jre-or-jdk
+# http://serverfault.com/questions/50883/what-is-the-value-of-java-home-for-centos
+# http://askubuntu.com/questions/175514/how-to-set-java-home-for-openjdk
+
 #download jdk
-#tar xf jdk*.tar.gz -C /usr/lib64
+fullfile="$1"
+#tar xf "$fullfile" -C /usr/lib64
 #cd /usr/lib64/jdk*/bin
-#for i in *; do ln -sf `pwd`/$i /usr/bin/; done
+#for i in *; do ln -sf $PWD/$i /usr/bin/; done
+
+# read env.adoc about env variables
+filename=$(basename "$fullfile")
+log "filename=$filename"
+#http://stackoverflow.com/questions/218156/bash-regex-with-quotes
+re="^(.*)\.tar\.[^.]+$"
+log "re=$re"
+if [[ "$filename" =~ $re ]]; then
+	dir="${BASH_REMATCH[1]}"
+else
+	echo "file should be of pattern <file.tar.[a-zA-Z]+>"
+	exit 1
+fi
+log "dir=$dir"
+cat > /etc/profile.d/java.sh <<EOF
+export JAVA_HOME=/usr/lib64/$dir
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+cat <<!
+Settings saved in /etc/profile.d/java.sh
+
+To update env run:
+
+source /etc/profile
+printenv JAVA_HOME
+!
